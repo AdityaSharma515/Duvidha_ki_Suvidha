26a455be3d21eeec61a91a4d42aae80d327444a9
SIDDHAARTHAA <siddhaartha09@gmail.com>
2025-11-05 19:10:15 +0000

    feat:upvote downvote implemented

--- Backend/src/middleware/auth.js
+++ Backend/src/middleware/auth.js
@@ -38,7 +38,6 @@ export function requireRole(role) {
       if (!req.user) {
         return res.status(401).json({ message: "Unauthorized" });
       }
-      console.log(req.user.role)
       if (req.user.role !== role) {
         return res.status(403).json({ message: "Unauthorized access" });
       }
