87a82640e98b5e112a5f7c10a977f71bd66fb4ce
SIDDHAARTHAA <siddhaartha09@gmail.com>
2025-11-16 03:18:59 +0000

    feat: server health route

--- Backend/server.js
+++ Backend/server.js
@@ -6,15 +6,17 @@ import cors from "cors"
 import { connectDB } from "./src/config/db.js";
 import authRoutes from "./src/routes/authroutes.js";
 import complaintRoutes from "./src/routes/Complaintroutes.js";
-const app=express()
+const app = express()
 app.use(cors());
 app.use(express.json())
 connectDB();
-app.use("/api/v1/auth", authRoutes);   
+app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/complaints", complaintRoutes);
+app.get("/", (req, res) => {
+    res.status(200).send("ok");
+});
 
-
-const PORT=process.env.PORT ||5001
-app.listen(PORT,()=>{
+const PORT = process.env.PORT || 5001
+app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
